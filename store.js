300fae21f2e7a3c4a1557e9c6641434d9e0b90d0
Tomer36 <67845283+Tomer36@users.noreply.github.com>
2021-07-02 05:15:31 +0300

    Update store.js

--- store.js
+++ store.js
@@ -131,9 +131,31 @@ function viewPrevPurchaces() {
         cache: false,
         success: function (data) {
             alert(data);
-            $('#table-container').html(data);
-            addItemToCart(data);
+            var dataSplit = data.split(' ');
+            $.each(dataSplit, function (index) {
+                addItemToCart(dataSplit[index]);
+
+            });
+
         }
+
+    });
+}
+function viewPrevPurchaces2() {
+    $.ajax({
+        url: "view_ajax2.php",
+        type: "POST",
+        cache: false,
+        success: function (data) {
+            alert(data);
+            var dataSplit = data.split(' ');
+            $.each(dataSplit, function (index) {
+                addItemToCart(dataSplit[index]);
+
+            });
+
+        }
+
     });
 }
 
@@ -261,6 +283,26 @@ function checkPass(event) {
     }
     if (isMatch && IsPass) {
         alert('Your Password have been changed!')
+    }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 }
 
