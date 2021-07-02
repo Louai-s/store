f7c8a741ee798d87f036a3d57139676abebd12ed
Louai-s <71526673+Louai-s@users.noreply.github.com>
2021-07-03 01:41:29 +0300

    Update Sign_up.php

--- Sign_up.php
+++ Sign_up.php
@@ -35,79 +35,47 @@
     if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
     if ($_POST) {
-        $users_Id = $_POST['Id']; //his id to access to account
-        $users_FirstName = $_POST['FirstName']; //his firstname
-        $users_LastName = $_POST['LastName']; //his lastname
-        $users_Birthday = $_POST['bday']; //his birthday
-        $users_AddressCity = $_POST['AddressCity']; //his address city
-        $users_AdressStreet = $_POST['AddressStreet']; //his address street
-        $users_PhoneNumber = $_POST['PhoneNumber']; //his phone number
-        $users_Gender = $_POST['Gender']; //if he is male or female
+        $users_Email = $_POST['Email']; //his id to access to account
+        $users_Confirm = $_POST['Confirm']; //his firstname
+        $users_Nickname = $_POST['Nickname']; //his lastname
+        $users_Phone = $_POST['Phone']; //his birthday
         $users_Password = $_POST['Password']; //password to access to account
+        
 
 
-        $query = "INSERT INTO client(Id, FirstName,LastName,Birthday,AddressCity,AddressStreet,PhoneNumber,Gender,Password) VALUES 
-  ('$users_Id','$users_FirstName','$users_LastName','$users_Birthday','$users_AddressCity','$users_AdressStreet','$users_PhoneNumber','$users_Gender','$users_Password')";
-        if ($conn->query($query) === TRUE) { //if did added to the clients
-    ?>
-            <script type='text/javascript'>
-                alert('Added !!');
-                window.location.href = "Sign_in.php";
-            </script>";
+            $query = "INSERT INTO client1(Email, Nickname,PhoneNumber,Password) VALUES 
+            ('$users_Email','$users_Nickname','$users_Phone','$users_Password')";
+            if ($conn->query($query) === TRUE) { //if did added to the clients
+            ?>
+                <script type='text/javascript'>
+                    alert('Added !!');
+                    window.location.href = "Sign_in.php";
+                </script>";
 
-        <?php
-        } else //if didnt added
+            <?php
+            } else //if didnt added
         {
-            echo "<script type='text/javascript'>alert('Error!!'); 
-window.history.back();
- </script>";
+            echo "<script type='text/javascript'>alert('Error!! You already have an account !'); 
+            window.history.back();
+            </script>";
         }
     } else {
         ?>
         <div id="SignUp">
 
             <form method="post">
-                <table align='center'>
-                    <tr>
-                        <td>ID</td>
-                        <td><input type="text" name="Id" pattern="[0-9]{3,}" title="Plese Enter Just a Numbers" required></td>
-                    </tr>
-                    <tr>
-                        <td>First name</td>
-                        <td><input type="text" name="FirstName" pattern="[A-Za-z]{2,}" title="First Name have numbers Or less than two charchter" required></td>
-                    </tr>
-                    <tr>
-                        <td>Last name</td>
-                        <td><input type="text" name="LastName" pattern="[A-Za-z]{2,}" title="First Name have numbers Or less than two charchter" required></td>
-                    </tr>
-                    <tr>
-                        <td>Birthday</td>
-                        <td><input type="date" value="2011-01-13" name="bday"></td>
-                    </tr>
-                    <tr>
-                        <td>Address City</td>
-                        <td><input type="text" name="AddressCity" required></td>
-                    </tr>
-                    <tr>
-                        <td>Adress Street</td>
-                        <td><input type="text" name="AddressStreet" required></td>
-                    </tr>
-                    <tr>
-                        <td>Phonenumber</td>
-                        <td><input type="text" name="PhoneNumber" pattern="[0-9]{10,10}" title="Enter 10 Numbers" maxlength="10" required></td>
-                    </tr>
-                    <tr>
-                        <td>Gender</td>
-                        <td><input type="text" name="Gender" required></td>
-                    </tr>
-                    <tr>
-                        <td>Pass</td>
-                        <td><input type="password" name="Password" required></td>
-                    </tr>
-                    <tr>
-                        <th colspan="2"><input type="submit" value="Submit"></th>
-                    </tr>
-                </table>
+            <h2 class="section-header" align="center" style="font-size: xx-large;"> Create New Account</h2>
+            <label for="Email">&nbsp&nbsp E-mail Address:</label><br>
+            &nbsp&nbsp<input type="Email" name="Email" id="Email"><br><br> 
+            <label for="Confirm">&nbsp&nbspConfirm E-mail Address:</label><br>
+            &nbsp&nbsp<input type="Email" name="Confirm" id="Confirm"><br><br>
+            <label for="Nickname">&nbsp&nbspNickname(Optional):</label><br>
+            &nbsp&nbsp<input type="text" name="Nickname" id="Nickname"><br><br>
+            <label for="Phone">&nbsp&nbspPhone Number(Optional):</label><br>
+            &nbsp&nbsp<input type="text" name="Phone" id="Phone"><br><br>
+            <label for="Password">&nbsp&nbsp Password:</label><br>
+            &nbsp&nbsp<input type="Password" name="Password" id="Password"><br><br>
+            <button class="btn btn-primary shop-item-button" onclick="checkEmail(event)" type="submit">Create</button>
             </form>
         </div>
         <br><br><br>
