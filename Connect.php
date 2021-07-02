3e731b8626c69971b68e5e8b97c6f0d9c787d318
Louai-s <71526673+Louai-s@users.noreply.github.com>
2021-07-03 01:45:46 +0300

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -13,25 +13,25 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error); //the result if we dont have connection to database
 }
 
-$us = $_POST["username"]; //set the username from textbox username
+$us = $_POST["Email"]; //set the username from textbox username
 $pass = $_POST["password"]; //set the password from textbox password
 
-$sql = "SELECT FirstName,LastName,Admin from client where ID='$us' AND Password='$pass'"; //searching for what username and password vaild
+$sql = "SELECT Email from client1 where Email='$us' AND Password='$pass'"; //searching for what username and password vaild
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) { //if result bigger than 0 thats mean password and username is vaild
   $row = $result->fetch_assoc();
 
-  $_SESSION["username"] = $us; //start session to username
+  $_SESSION["Email"] = $us; //start session to username
   $_SESSION["password"] = $pass; //start session to password
-  $_SESSION["FN"] = $row["FirstName"]; //start session of name to print it
-  $_SESSION["LN"] = $row["LastName"]; //start session of lastname to print it!
-  $_SESSION["Admin"] = $row["Admin"]; //if he an admin the session value will be "1"!
-  if (isset($_COOKIE[$_SESSION['username']])) //if we have an cookie we put the cookie in session array of items!
+  $_SESSION["Email"] = $row["Email"]; //start session of name to print it
+  //$_SESSION["LN"] = $row["LastName"]; //start session of lastname to print it!
+  //$_SESSION["Admin"] = $row["Admin"]; //if he an admin the session value will be "1"!
+  if (isset($_COOKIE[$_SESSION['Email']])) //if we have an cookie we put the cookie in session array of items!
   {
-    $cart = $_COOKIE[$_SESSION['username']];
+    $cart = $_COOKIE[$_SESSION['Email']];
     $_SESSION['cart'] = unserialize($cart);
-    $total = $_COOKIE[$_SESSION['username'] . "1"];
+    $total = $_COOKIE[$_SESSION['Email'] . "1"];
     $_SESSION['total'] = $total;
   } else {
     $_SESSION['total'] = 0; //if we dont have and cookie we start with 0 items in the card
